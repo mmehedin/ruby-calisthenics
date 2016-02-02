ce24859fa15272ce924f65fb28a04f8ab218e46b
mmehedin <mmehedin@uncc.edu>
2016-02-02 16:49:54 +0000

    HW1 solution

--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,9 +1,22 @@
 module FunWithStrings
   def palindrome?
     # your code here
+    self.downcase!
+    @s= self.gsub(/[^a-zA-Z]/, '')
+    #self.match(/[a-zA-Z]/)
+    @s==@s.reverse
   end
+  
   def count_words
     # your code here
+    #s.split.size
+    @s =self.gsub(/[^a-zA-Z\s]/, '')
+    hash = Hash.new(0)
+    array=@s.downcase.split(' ')
+    array.each {|word| hash[word]+=1}
+    hash
+   # numb = WordsCounted::Counter.new(self)
+    #numb.word_count
   end
   def anagram_groups
     # your code here
@@ -14,4 +27,12 @@ end
 
 class String
   include FunWithStrings
+=begin
+def initialize(s)
+    @s = s
+    s.palindrome?(@s)
+  end;
+=end 
+
 end
+
